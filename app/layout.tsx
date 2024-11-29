c08d991bcacaac566be0aac5361590695a0063a2
JunmanChoi <junman9595@gmail.com>
2024-11-29 17:40:24 +0900

    chore: radix/ui 설치 및 샘플 다이얼로그 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -27,8 +29,10 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        >
+        <Theme appearance="dark">
         {children}
+      </Theme>
       </body>
     </html>
   );
